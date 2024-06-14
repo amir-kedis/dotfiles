4b0e59acf26d1c4595f558a2444e7d4cb0d54657
amir-kedis <amir.kedis@gmail.com>
2024-06-14 11:49:05 +0300

    feat(nvim): add C support

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -25,6 +25,7 @@ return { -- Autoformat
 		end,
 		formatters_by_ft = {
 			lua = { "stylua" },
+			c = { "clang-format" },
 			-- Conform can also run multiple formatters sequentially
 			-- python = { "isort", "black" },
 			--
