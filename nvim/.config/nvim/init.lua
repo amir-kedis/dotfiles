7b9cd4b9b71e2ec2b8e6927e07db9f88cd57191a
amir-kedis <amir.kedis@gmail.com>
2024-06-14 09:30:31 +0300

    refactor(nvim): seprate autocmds into seprate file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -100,18 +100,7 @@ require("options")
 require("keymaps")
 
 -- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
+require("autocmds")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
