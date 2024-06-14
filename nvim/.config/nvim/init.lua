fb9b021e1ee6b8c5c8c8276175e7c08bfd655207
amir-kedis <amir.kedis@gmail.com>
2024-06-14 11:30:31 +0300

    refactor(nvim): plugins into seprate files for easier config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -26,9 +26,5 @@ require("lazy-bootstrap")
 -- [[ Configure and install plugins ]]
 require("lazy-plugins")
 
--- [[ Add health file ]]
--- NOTE: still don't know what it does exactly.
-require("health")
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,26 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	cmd = "Neotree",
+	keys = {
+		{ "\\", ":Neotree reveal<CR>", { desc = "NeoTree reveal" } },
+	},
+	opts = {
+		filesystem = {
+			window = {
+				mappings = {
+					["\\"] = "close_window",
+				},
+			},
+		},
+	},
+}
+-- vim: ts=2 sts=2 sw=2 et
