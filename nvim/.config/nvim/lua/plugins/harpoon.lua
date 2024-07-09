fcd2948413b67e479617ce1584a4b6cee05f00e3
amir-kedis <amir.kedis@gmail.com>
2024-07-09 17:46:45 +0300

    feat: add harpoon finally

---
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,68 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim",
+	},
+	config = function()
+		require("harpoon"):setup()
+	end,
+	keys = {
+		{
+			"<leader>m",
+			function()
+				require("harpoon"):list():add()
+			end,
+			desc = "harpoon mark file",
+		},
+		{
+			"<leader>a",
+			function()
+				local harpoon = require("harpoon")
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end,
+			desc = "harpoon quick menu",
+		},
+		{
+			"<leader>1",
+			function()
+				require("harpoon"):list():select(1)
+			end,
+			desc = "harpoon to file 1",
+		},
+		{
+			"<leader>2",
+			function()
+				require("harpoon"):list():select(2)
+			end,
+			desc = "harpoon to file 2",
+		},
+		{
+			"<leader>3",
+			function()
+				require("harpoon"):list():select(3)
+			end,
+			desc = "harpoon to file 3",
+		},
+		{
+			"<leader>4",
+			function()
+				require("harpoon"):list():select(4)
+			end,
+			desc = "harpoon to file 4",
+		},
+		{
+			"<leader>5",
+			function()
+				require("harpoon"):list():select(5)
+			end,
+			desc = "harpoon to file 5",
+		},
+		{
+			"<leader>sm",
+			"<CMD>Telescope harpoon marks<CR>",
+			desc = "Search Harpoon Marks",
+		},
+	},
+}
