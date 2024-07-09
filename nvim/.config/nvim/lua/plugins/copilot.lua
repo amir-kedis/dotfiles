2b141506725efb2d867290aa958ba0090a5c64f0
amir-kedis <amir.kedis@gmail.com>
2024-07-09 17:16:03 +0300

    feat: disable copilot

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,4 +1,17 @@
 -- copilot configr
 return {
 	"github/copilot.vim",
+
+	config = function()
+		-- change the default keymap
+		vim.keymap.set(
+			"i",
+			"<C-y>",
+			'copilot#Accept("<CR>")',
+			{ expr = true, noremap = true, silent = true, replace_keycodes = false }
+		)
+		vim.keymap.set("i", "<C-L>", "<Plug>(copilot-accept-line)", { noremap = false })
+		vim.keymap.set("i", "<C-Space>", "<Plug>(copilot-suggest)", { noremap = false })
+	end,
+	disable = true,
 }
